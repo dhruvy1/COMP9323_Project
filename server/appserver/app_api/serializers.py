9b93b990d86606f617569b89eff7b086c3ddc36c
Dhruv <dhruvk995@gmail.com>
2017-09-06 22:40:34 +1000

    Basic API completed

--- server/appserver/app_api/serializers.py
+++ server/appserver/app_api/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import *
 
 
+class MobileUserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MobileUser
+        fields = ['username', 'device_id']
+
+
 class MobileUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MobileUser
-        fields = ['username', 'device_id',]
+        fields = ['id', 'username', 'device_id']
+
--- server/appserver/app_api/urls.py
+++ server/appserver/app_api/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'mobile_user/$', MobileUserAPIView.as_view(), name='list'),
+    url(r'mobile_user_detail/create/$', MobileUserCreateAPIView.as_view(), name='create'),
+    url(r'mobile_user_detail/(?P<pk>[0-9]+)/$', MobileUserDetailAPIView.as_view(), name='detail'),
+    url(r'mobile_user_detail/(?P<pk>[0-9]+)/edit/$', MobileUserUpdateAPIView.as_view(), name='update'),
+    url(r'mobile_user_detail/(?P<pk>[0-9]+)/delete/$', MobileUserDestroyAPIView.as_view(), name='delete'),
+
 ]
