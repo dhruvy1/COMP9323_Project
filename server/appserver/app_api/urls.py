45644659cf04db76d878fd0476a300c3f79e998f
Dhruv <dhruvk995@gmail.com>
2017-09-09 15:30:09 +1000

    Added API Docs

--- server/appserver/app_api/urls.py
+++ server/appserver/app_api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from .views import *
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='9323 API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^$', index, name='index'),
     url(r'mobile_user/$', MobileUserAPIView.as_view(), name='list'),
     url(r'mobile_user_detail/create/$', MobileUserCreateAPIView.as_view(), name='create'),
