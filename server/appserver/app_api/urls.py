75fce52285a4d0e783d6dc26a65cbf58b776fae9
Dhruv <dhruvk995@gmail.com>
2017-09-09 19:25:45 +1000

    Updated API to follow REST conventions

--- server/appserver/app_api/urls.py
+++ server/appserver/app_api/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     # Index URL
     url(r'^$', index, name='index'),
     # mobile_user list URL
-    url(r'mobile_user/$', MobileUserAPIView.as_view(), name='list'),
+    url(r'mobile_users/$', MobileUserAPIView.as_view(), name='list'),
     # mobile_user_detail URLs
-    url(r'mobile_user_detail/create/$', MobileUserCreateAPIView.as_view(), name='create'),
-    url(r'mobile_user_detail/(?P<pk>[0-9]+)/$', MobileUserDetailAPIView.as_view(), name='detail'),
-    url(r'mobile_user_detail/(?P<pk>[0-9]+)/edit/$', MobileUserUpdateAPIView.as_view(), name='update'),
-    url(r'mobile_user_detail/(?P<pk>[0-9]+)/delete/$', MobileUserDestroyAPIView.as_view(), name='destroy'),
+    url(r'mobile_users/create/$', MobileUserCreateAPIView.as_view(), name='create'),
+    url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserDetailAPIView.as_view(), name='detail'),
+    url(r'mobile_users/(?P<pk>[0-9]+)/edit/$', MobileUserUpdateAPIView.as_view(), name='update'),
+    url(r'mobile_users/(?P<pk>[0-9]+)/delete/$', MobileUserDestroyAPIView.as_view(), name='destroy'),
     # Event urls
-    url(r'event/all/$', EventListAPIView.as_view(), name='list'),
-    url(r'event/(?P<pk>[0-9]+)/$', EventDetailAPIView.as_view(), name='detail'),
-    url(r'event/create/$', EventCreateAPIView.as_view(), name='create'),
-    url(r'event/(?P<pk>[0-9]+)/$', EventUpdateAPIView.as_view(), name='update'),
-    url(r'event/(?P<pk>[0-9]+)/$', EventDestroyAPIView.as_view(), name='destroy'),
+    url(r'events/$', EventListAPIView.as_view(), name='list'),
+    url(r'events/(?P<pk>[0-9]+)/$', EventDetailAPIView.as_view(), name='detail'),
+    url(r'events/create/$', EventCreateAPIView.as_view(), name='create'),
+    url(r'events/(?P<pk>[0-9]+)/$', EventUpdateAPIView.as_view(), name='update'),
+    url(r'events/(?P<pk>[0-9]+)/$', EventDestroyAPIView.as_view(), name='destroy'),
 ]
