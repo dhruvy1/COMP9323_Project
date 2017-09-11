1a450843fdc95b099c2559e1e325489b27088a3c
Dhruv <dhruvk995@gmail.com>
2017-09-11 20:15:05 +1000

    Updated API to be REST compliant

--- server/appserver/app_api/urls.py
+++ server/appserver/app_api/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
     # mobile_user list URL
     url(r'mobile_users/$', MobileUserAPIView.as_view(), name='list'),
     # mobile_user_detail URLs
-    url(r'mobile_users/create/$', MobileUserCreateAPIView.as_view(), name='create'),
+    url(r'mobile_users/$', MobileUserCreateAPIView.as_view(), name='create'),
     url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserDetailAPIView.as_view(), name='detail'),
-    url(r'mobile_users/(?P<pk>[0-9]+)/edit/$', MobileUserUpdateAPIView.as_view(), name='update'),
-    url(r'mobile_users/(?P<pk>[0-9]+)/delete/$', MobileUserDestroyAPIView.as_view(), name='destroy'),
+    url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserUpdateAPIView.as_view(), name='update'),
+    url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserDestroyAPIView.as_view(), name='destroy'),
     # Event urls
     url(r'events/$', EventListAPIView.as_view(), name='list'),
     url(r'events/(?P<pk>[0-9]+)/$', EventDetailAPIView.as_view(), name='detail'),
-    url(r'events/create/$', EventCreateAPIView.as_view(), name='create'),
+    url(r'events/$', EventCreateAPIView.as_view(), name='create'),
     url(r'events/(?P<pk>[0-9]+)/$', EventUpdateAPIView.as_view(), name='update'),
     url(r'events/(?P<pk>[0-9]+)/$', EventDestroyAPIView.as_view(), name='destroy'),
 ]
