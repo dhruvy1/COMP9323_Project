abcb3aa845f36bac2c1dc3646abf7ed35a5765c7
Dhruv <dhruvk995@gmail.com>
2017-09-12 23:37:18 +1000

    URL fix

--- server/appserver/app_api/urls.py
+++ server/appserver/app_api/urls.py
@@ -9,20 +9,20 @@ urlpatterns = [
     # Index URL
     url(r'^$', index, name='index'),
     # mobile_user list URL
-    url(r'mobile_users/$', MobileUserAPIView.as_view(), name='list'),
+    url(r'mobile_users/all/$', MobileUserAPIView.as_view(), name='list'),
     # mobile_user_detail URLs
     url(r'mobile_users/$', MobileUserCreateAPIView.as_view(), name='create'),
     url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserDetailAPIView.as_view(), name='detail'),
     url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserUpdateAPIView.as_view(), name='update'),
     url(r'mobile_users/(?P<pk>[0-9]+)/$', MobileUserDestroyAPIView.as_view(), name='destroy'),
     # Event urls
-    url(r'events/$', EventListAPIView.as_view(), name='list'),
+    url(r'events/all/$', EventListAPIView.as_view(), name='list'),
     url(r'events/$', EventCreateAPIView.as_view(), name='create'),
     url(r'events/(?P<pk>[0-9]+)/$', EventDetailAPIView.as_view(), name='detail'),
     url(r'events/(?P<pk>[0-9]+)/$', EventUpdateAPIView.as_view(), name='update'),
     url(r'events/(?P<pk>[0-9]+)/$', EventDestroyAPIView.as_view(), name='destroy'),
     # Food Deals
-    url(r'food_deals/$', FoodDealListAPIView.as_view(), name='list'),
+    url(r'food_deals/all/$', FoodDealListAPIView.as_view(), name='list'),
     url(r'food_deals/$', FoodDealCreateAPIView.as_view(), name='create'),
     url(r'food_deals/(?P<pk>[0-9]+)/$', FoodDealDetailAPIView.as_view(), name='detail'),
     url(r'food_deals/(?P<pk>[0-9]+)/$', FoodDealUpdateAPIView.as_view(), name='update'),
