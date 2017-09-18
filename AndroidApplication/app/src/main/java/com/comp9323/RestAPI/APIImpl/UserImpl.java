2c40e0879bacbe7747c01c64c0185689413bb7e8
Thomas Wong <thomas@DESKTOP-FBK4PQG.localdomain>
2017-09-18 17:34:35 +1000

    before merge

--- AndroidApplication/app/src/main/java/com/comp9323/RestAPI/APIImpl/UserImpl.java
+++ AndroidApplication/app/src/main/java/com/comp9323/RestAPI/APIImpl/UserImpl.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import java.util.HashMap;
 import java.util.UUID;
+import java.util.Vector;
 
 import com.comp9323.RestAPI.APIInterface.RestClient;
 import com.comp9323.RestAPI.APIInterface.UserInterface;
@@ -15,6 +17,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class UserImpl {
     public static final String USR_PERF = "APP_USR_INFO";
     private static final UserInterface apiInterface = RestClient.getClient().create(UserInterface.class);
@@ -23,14 +26,97 @@ public class UserImpl {
         CreateUser(new User(username, createUUID()));
     }
 
-    /**
-     * @param user
-     */
+    public static boolean deleteSelf() {
+        return deleteUser(MainActivity.DH.getUserSelf().getId());
+    }
+
+    public static boolean editUserWithFields(HashMap<String, String> couples) {
+        //create new user template
+        int Id = MainActivity.DH.getUserSelf().getId();
+        User templateUser = new User();
+        for (String item : couples.keySet()) {
+            String value = couples.get(item);
+            if (item.equals("username")) {
+                templateUser.setUsername(value);
+            } else if (item.equals("deviceid")) {
+                templateUser.setDeviceId(value);
+            }
+        }
+        //call Rest
+        return editUserWithFields(Id, templateUser);
+    }
+
+    public static boolean editWholeUser(HashMap<String, String> couples) {
+        //load user
+        User userSelf = MainActivity.DH.getUserSelf();
+        //set fields
+        for (String item : couples.keySet()) {
+            String value = couples.get(item);
+            if (item.equals("username") ){
+                userSelf.setUsername(value);
+            } else if (item.equals( "deviceid") ){
+                userSelf.setDeviceId(value);
+            }
+        }
+        //call Rest
+        return editWholeUser(userSelf.getId(), userSelf);
+
+    }
+
+    public static boolean getUsers() {
+        Log.v("Rest Call", "Start Load All User");
+        final boolean[] ifSuccess = {false};
+        apiInterface.getUsers().enqueue(new Callback<Vector<User>>() {
+            @Override
+            public void onResponse(Call<Vector<User>> call, Response<Vector<User>> response) {
+                Log.d("Rest Call", "Is response success? " + response.isSuccessful());
+                Vector<User> users = response.body();
+                if (users != null) {
+                    MainActivity.DH.addUsers(users);
+                    ifSuccess[0] = true;
+                    for (User u : users) {
+                        Log.d("Rest Debug Print", u.toString());
+                    }
+                }
+                Log.v("Rest Call", "End get User list");
+            }
+
+            @Override
+            public void onFailure(Call<Vector<User>> call, Throwable t) {
+                Log.d("Rest Call", "~~FAILED~~");
+                call.cancel();
+            }
+        });
+        return ifSuccess[0];
+    }
+
+    public static boolean getUser(int id) {
+        Log.v("Rest Call", "Start Get User");
+        final boolean[] ifSuccess = {false};
+        apiInterface.getUser(id).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Log.d("LOG_TAG", "Is response success? " + response.isSuccessful());
+                User user = response.body();
+                Log.d("Rest Debug print", user.toString());
+                MainActivity.DH.addUser(user);//TODO Should i store it?
+                ifSuccess[0] = true;
+                Log.v("Rest Call", "End Get User");
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d("REST CALL", "~~FAILED~~");
+                call.cancel();
+            }
+        });
+        return ifSuccess[0];
+    }
+
     private static void CreateUser(User user) {
         //final SingletonDataHolder DH = SingletonDataHolder.getInstance();
-        Log.v("TAG", "start calling REST");
-        Call<User> call = apiInterface.createUser(user);
-        call.enqueue(new Callback<User>() {
+        Log.v("Rest Call", "Start Create User");
+        apiInterface.createUser(user).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 Log.d("LOG_TAG", "Is response success? " + response.isSuccessful());
@@ -43,17 +129,76 @@ public class UserImpl {
                 SFE.putString("username", user.getUsername());
                 SFE.putString("uuid", user.getDeviceId());
                 SFE.commit();
-                return;
+                Log.v("Rest Call", "End Create User");
             }
 
             @Override
             public void onFailure(Call call, Throwable t) {
-                Log.d("REST CALL", "FAILED");
+                Log.d("REST CALL", "~~FAILED~~");
                 call.cancel();
             }
         });
     }
 
+    private static boolean deleteUser(int id) {
+        Log.v("Rest Call", "Start Delete User (self)");
+        final boolean[] ifSuccess = {false};
+        apiInterface.deleteUser(id).enqueue(new Callback<Response<Void>>() {
+            @Override
+            public void onResponse(Call<Response<Void>> call, Response<Response<Void>> response) {
+                Log.d("LOG_TAG", "Is response success? " + response.isSuccessful());
+                ifSuccess[0] = response.isSuccessful();
+                Log.v("Rest Call", "End Delete User(self)");
+            }
+
+            @Override
+            public void onFailure(Call<Response<Void>> call, Throwable t) {
+                Log.d("REST CALL", "~~FAILED~~");
+                call.cancel();
+            }
+        });
+        return ifSuccess[0];
+    }
+
+    private static boolean editUserWithFields(int id, User user) {
+        Log.v("Rest Call", "start Edit Fields");
+        final boolean[] ifSuccess = {false};
+        apiInterface.editUserWithFields(id, user).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Log.d("LOG_TAG", "Is response success? " + response.isSuccessful());
+                ifSuccess[0] = response.isSuccessful();
+                Log.v("Rest Call", "End Edit Fields");
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d("REST CALL", "~~FAILED~~");
+                call.cancel();
+            }
+        });
+        return ifSuccess[0];
+    }
+
+    private static boolean editWholeUser(int id, User user) {
+        Log.v("Rest Call", "start Edit User");
+        final boolean[] ifSuccess = {false};
+        apiInterface.editWholeUser(id, user).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Log.d("LOG_TAG", "Is response success? " + response.isSuccessful());
+                ifSuccess[0] = response.isSuccessful();
+                Log.v("Rest Call", "End Edit User");
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d("REST CALL", "~~FAILED~~");
+                call.cancel();
+            }
+        });
+        return ifSuccess[0];
+    }
 
     private static String createUUID() {
         return UUID.randomUUID().toString();
--- AndroidApplication/app/src/main/java/com/comp9323/RestAPI/DataHolder/SingletonDataHolder.java
+++ AndroidApplication/app/src/main/java/com/comp9323/RestAPI/DataHolder/SingletonDataHolder.java
@@ -3,7 +3,14 @@ package com.comp9323.RestAPI.DataHolder;
 import android.content.Context;
 import android.util.Log;
 
+import com.comp9323.RestAPI.Beans.FoodDeal;
 import com.comp9323.RestAPI.Beans.User;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 /**
  * NOTE: Everything store in this class  WILL BE GONE when the application is kill
@@ -19,8 +26,12 @@ public class SingletonDataHolder {
     //Map<String, WeakReference<Object>> or Vector<WeakReference<Object>> objectData; //using "weakreference" allow destroy data when quit activities
     //vector<Object> ObjectData;
     //remember to initialize object list/map
+    private List< WeakReference<User>> userList;
+    private List<WeakReference<FoodDeal>> foodDealList;
 
     private SingletonDataHolder() {
+        userList = new Vector<>();
+        foodDealList = new Vector<>();
         //...
     }
 
@@ -44,4 +55,53 @@ public class SingletonDataHolder {
     public void setContext(Context context) {
         this.context = context;
     }
+
+    public WeakReference<User> getUser(int Index){
+        return userList.get(Index);
+    }
+    public List<WeakReference<User>> getUserList(){
+        return userList;
+    }
+    public void addUser(User user){
+        userList.add(new WeakReference<>(user));
+    }
+    public void addUsers(List<User> users){
+        for (User user : users) {
+            userList.add(new WeakReference<>(user));
+        }
+    }
+    public void removeUser(int Index){
+        userList.remove(Index);
+    }
+    public void clearUserList(){
+        userList.clear();
+    }
+    public WeakReference<FoodDeal> getFoodDeal(int Index){
+        return foodDealList.get(Index);
+    }
+    public WeakReference<FoodDeal> getFoodDealwithID(int id){
+        for (WeakReference<FoodDeal> ref: foodDealList) {
+            if(ref.get().getId() == id)
+                return ref;
+        }
+        return null;
+    }
+    public List<WeakReference<FoodDeal>> getFoodDealList(){
+        return foodDealList;
+    }
+    public void addFoodDeal(FoodDeal fd){
+        foodDealList.add(new WeakReference<>(fd));
+    }
+    public void addFoodDeals(List<FoodDeal> fds){
+        for (FoodDeal fd: fds) {
+            foodDealList.add(new WeakReference<>(fd));
+        }
+    }
+    public void removeFoodDeall(int Index){
+        foodDealList.remove(Index);
+    }
+    public void clearFoodList(){
+        foodDealList.clear();
+    }
+
 }
