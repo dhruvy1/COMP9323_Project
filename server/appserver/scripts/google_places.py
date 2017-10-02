6deda4dad3648c6a4e1ba636ddbd915c96e158f7
Lucas Leong <lucasleong@hotmail.com>
2017-10-03 10:38:55 +1100

    Added photos to Food places

--- server/appserver/scripts/google_places.py
+++ server/appserver/scripts/google_places.py
@@ -4,7 +4,8 @@ import json
 import requests
 import time
 
-gmaps = googlemaps.Client(key='AIzaSyCkWimLuZg74e53MSlegLb3ek65bR6I8Qw')
+API_KEY = "AIzaSyCkWimLuZg74e53MSlegLb3ek65bR6I8Qw"
+gmaps = googlemaps.Client(key=API_KEY)
 
 
 def getFoodPlacesNearUNSW(next_page_token):
@@ -12,6 +13,7 @@ def getFoodPlacesNearUNSW(next_page_token):
     unsw_coordinates = "-33.9173425, 151.2290788"
     places_nearby_results = gmaps.places_nearby(
         unsw_coordinates, 1000, type="food", page_token=next_page_token)
+
     for place in places_nearby_results["results"]:
         payload = {
             "name": place["name"],
@@ -23,6 +25,8 @@ def getFoodPlacesNearUNSW(next_page_token):
             payload["price_level"] = str(place["price_level"])
         if "rating" in place:
             payload["google_rating"] = str(place["rating"])
+        if "photos" in place:
+            payload["photo"] = getGooglePhotoUrl(place["photos"][0])
         postToRestServer(payload)
         # print(payload)
         # print(places_nearby_results)
@@ -31,12 +35,20 @@ def getFoodPlacesNearUNSW(next_page_token):
         getFoodPlacesNearUNSW(places_nearby_results["next_page_token"])
 
 
+def getGooglePhotoUrl(photo):
+    url = "https://maps.googleapis.com/maps/api/place/photo?photoreference="
+    url += photo["photo_reference"]
+    url += "&maxheight=" + str(photo["height"]) + "&maxwidth=" + str(photo["width"])
+    url += "&key=" + API_KEY
+    return url
+
+
 def postToRestServer(payload):
     url = "http://52.65.129.3:8000/api/food_places/"
     headers = {"Content-Type": "application/json",
                "Accept": "application/json"}
     response = requests.post(url, data=json.dumps(payload), headers=headers)
-    # print(response)
+    print(response)
 
 
 if __name__ == "__main__":
