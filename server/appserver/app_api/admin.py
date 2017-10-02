7d23563b2e4e2ff2807fdf5d388059d4875ca8c6
Dhruv <dhruvk995@gmail.com>
2017-10-02 22:47:23 +1100

    admin for FoodPlace

--- server/appserver/app_api/admin.py
+++ server/appserver/app_api/admin.py
@@ -5,3 +5,8 @@ from .models import *
 class MobileUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'device_id')
 admin.site.register(MobileUser, MobileUserAdmin)
+
+
+class FoodPlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'location', 'price_level', 'google_rating', 'latitude', 'longitude')
+admin.site.register(FoodPlace, FoodPlaceAdmin)
