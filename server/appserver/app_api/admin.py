cf3c827ace7e90886f58fbc7bbcd4f6082b24cc4
Dhruv <dhruvk995@gmail.com>
2017-10-04 15:06:39 +1100

    FoodDealAdmin and EventAdmin added

--- server/appserver/app_api/admin.py
+++ server/appserver/app_api/admin.py
@@ -10,3 +10,15 @@ admin.site.register(MobileUser, MobileUserAdmin)
 class FoodPlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'location', 'price_level', 'google_rating', 'latitude', 'longitude')
 admin.site.register(FoodPlace, FoodPlaceAdmin)
+
+
+class FoodDealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post_id', 'message', 'updated_time', 'photo_link', 'event_link', 'updated_date', 'rating', 'created_by')
+admin.site.register(FoodDeal, FoodDealAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'start_time', 'facebook_id', 'start_date', 'end_date', 'end_time',
+                  'place_name', 'city', 'country', 'latitude', 'longitude', 'state', 'street', 'zip',
+                  'cover_id', 'source_url', 'rating', 'created_by')
+admin.site.register(Event, EventAdmin)
