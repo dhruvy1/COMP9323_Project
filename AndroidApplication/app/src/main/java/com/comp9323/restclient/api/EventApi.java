ffb42daa4192fb1e7911105e12bab7778310cdff
ttan100 <31614229+ttan100@users.noreply.github.com>
2017-10-18 14:44:05 +1100

    accidentally deleted a file

---
+++ AndroidApplication/app/src/main/java/com/comp9323/restclient/api/EventApi.java
@@ -0,0 +1,38 @@
+package com.comp9323.restclient.api;
+
+import com.comp9323.data.beans.Event;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * Created by timta on 11/09/2017.
+ */
+
+public interface EventApi {
+    @GET("events/{id}")
+    Call<Event> getEvent(@Path("id") int eventId);
+
+    @GET("events/all/")
+    Call<List<Event>> getEvents();
+
+    @POST("events/")
+    Call<Event> postEvent(@Body Event event);
+
+    @DELETE("events/{id}")
+    Call<Void> deleteEvent(@Path("id") int eventId);
+
+    @PUT("events/{id}")
+    Call<Event> putEvent(@Path("id") int eventId, @Body Event event);
+
+    @PATCH("events/{id}")
+    Call<Event> patchEvent(@Path("id") int eventId, @Body Event event);
+}
